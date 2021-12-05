bd2f4682f26d3c3635e3df31f1e7de71caaca12f
Ahadjimalis <83683366+Ahadjimalis@users.noreply.github.com>
2021-12-04 20:29:49 -0800

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,10 +1,28 @@
+import os
 class Server:
     """ Server class for representing and manipulating servers. """
 
     def __init__(self, server_ip):
         # TODO -
         self.server_ip = server_ip
+        self.ping()
 
     def ping(self):
         # TODO - Use os module to ping the server
-        return
+        print("The Server IP is", self.server.ip)
+        filepath == os.path.dirname(os.path.abspath(_file__))
+        os.chdir(filepath)
+
+
+        response = os.system("ping -c 1 -w 5 " + self.server_ip + " > /dev/null 2>&1")
+
+        if response == 0:
+            return (self.server_ip + "is up and Running")
+        else:
+            return (self.server_ip + "is Down")
+
+
+server_ip("3.139.101.224")
+print(server.ping())
+
+
