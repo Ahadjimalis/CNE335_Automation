04ea062c490ca46e6d407e5e74d3563af88db7a9
Ahadjimalis <83683366+Ahadjimalis@users.noreply.github.com>
2021-12-04 21:32:07 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 # This is the template code for the CNA337 Final Project
 # Zachary Rubin, zrubin@rtc.edu
 # CNA 337 Fall 2020
-import os
+
+import subprocess
+import platform
+
 def print_program_info():
     # TODO - Change your name
     print("Server Automator v0.1 by Albert Hadjimalis")
@@ -9,16 +12,18 @@ def print_program_info():
 # This is the entry point to our program
 if __name__ == '__main__':
     print_program_info()
+    
     # TODO - Create a Server object
 
 
 def ping(host):
-    param = '-n' if platform.system().lower()=='windows' else '-c'
-        return True
-    else:
-        return False
+    param = '-n' if platform.system().lower()== 'windows' else '-c'
+    command = ['ping', param, '1', host]
+    return subprocess.call(command)
+    
 
-print(myping("ec2-3-139-101-224.us-east-2.compute.amazonaws.com"))
+host = 'ec2-3-139-101-224.us-east-2.compute.amazonaws.com'
+print(ping(host))
 
-#https://www.delftstack.com/howto/python/python-ping/
-#https://www.py4u.net/discuss/11338
+
+
